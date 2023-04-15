2880fabc5e843a9e21998befe927309320a82e7f
Mohamed Abozaid <mohamed.abozaid01@gmail.com>
2023-04-15 14:51:02 +0200

    connect to DB and create schema, model, and document

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: `${__dirname}/.env` });
 
-console.log(app.get('env'));
+const mongosse = require('mongoose');
+const app = require('./app');
+
+const connString = process.env.DATABASE;
+mongosse
+    .connect(connString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log('connected to DB sucessfully ...');
+    })
+    .catch((err) => console.log('ERROR!!!', err));
+
+const tourSchema = new mongosse.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    rating: { type: Number, default: 4.5 },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+});
+
+const Tour = mongosse.model('Tour', tourSchema);
+
+const tour = new Tour({
+    name: 'The Forest Hiker',
+    price: 300,
+    rating: 4.8,
+});
+
+tour.save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((err) => {
+        console.log('ERROR!', err);
+    });
 
 //Starting the server
 const port = process.env.PORT || 3000;
